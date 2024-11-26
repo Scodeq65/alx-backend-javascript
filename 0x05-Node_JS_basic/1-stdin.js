5c436184b851d626759b81c29e570405ed548df2
scodeq65 <sodiqagbaraojo@gmail.com>
2024-11-26 09:08:07 +0100

    modified

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 // Display the welcome message
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 // Handle User input stdin
 process.stdin.on('data', (data) => {
-    const input = data.toString().trim();
-    console.log(`Your name is: ${input}`);
+  const input = data.toString().trim();
+  console.log(`Your name is: ${input}`);
 
-    // Exit the program gracefully
-    console.log("This important software is closing");
-    process.exit();
+  // Exit the program gracefully
+  console.log('This important software is closing');
+  process.exit();
 });
