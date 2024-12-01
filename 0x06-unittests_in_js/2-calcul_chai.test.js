71dbdd43fc3fb2d26bd7fc015e9ad948431c36cb
scodeq65 <sodiqagbaraojo@gmail.com>
2024-12-01 05:41:58 +0100

    chai

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,38 @@
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return the sum of two rounded numbers', () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+      expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
+      expect(calculateNumber('SUM', -1.2, -3.7)).to.equal(-5);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return the difference of two rounded numbers', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+      expect(calculateNumber('SUBTRACT', 5.5, 3.2)),to.equal(3);
+      expect(calculateNumber('SUBTRACT', -1.2, -3.7)).to.equal(3);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return the division of two rounded numbers', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+      expect(calculateNumber('DIVIDE', 5.5, 2.2)).to.equal(3);
+    });
+
+    it('should return "Error" when dividing by zero', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 3.7, 0)).to.equal('Error');
+    });
+  });
+
+  describe('Invalid type', () => {
+    it('should throw an error for invalid operation types', () => {
+      expect(() => calculateNumber('MULTIPLY', 1.4, 4.5)).to.throw('Invalid operation type');
+    });
+  });
+});
